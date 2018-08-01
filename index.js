771a42e5c60c725286a793f80d575c48a14f705e
mishanya <the.one.mishanya@gmail.com>
2018-08-01 15:04:53 +0300

    changed ports

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     // res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, application/json');
+    res.header('Access-Control-Allow-Headers', '*');
 
     next();
 });
