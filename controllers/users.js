50c92be5744776ac6f3f71a07abe1fcae75bc6db
Aleksandr Volkov <avolkov05@gmail.com>
2018-08-07 16:41:26 +0700

    fix get address

--- controllers/users.js
+++ controllers/users.js
@@ -73,8 +73,13 @@ module.exports = {
             })
             .catch(error => res.status(400).json({message: error}));
     },
-    getAddress(req, res) {
-        return Wallet.findOne({where: {user_id: req.user.id}})
+    async getAddress(req, res) {
+        let user_id = req.body.user_id;
+        let user = await User.findById(user_id);
+        if (!user) return res.status(404).json({
+            message: 'User Not Found',
+        });
+        return Wallet.findOne({where: {user_id: user_id}})
             .then(wallet => {
 
                 if (!wallet) {
