d4e7afe1a599ce122f4ad849553c3764141ddf47
Aleksandr Volkov <avolkov05@gmail.com>
2018-09-11 21:26:28 +0700

    fix

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     if (allowedOrigins.indexOf(origin) > -1) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', '*');
 
     next();
