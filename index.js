3340dacb25a4b04734297d224d4635f19605ef0b
Aleksandr Volkov <a.volkov@xdsoft.ru>
2018-09-13 13:17:53 +0700

    aa

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', '*');
 
-    if (req.headers['X-App-Key'] == 'lucky'){
-        res.writeHead('Access-Control-Allow-Origin', origin);
-    }
+    // if (req.headers['X-App-Key'] == 'lucky'){
+        // res.writeHead('Access-Control-Allow-Origin', origin);
+    // }
 
     next();
 });
