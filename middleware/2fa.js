d101b7fc3f5c4bf70ac7667d5f98ecf8a3d87de5
Aleksandr Volkov <a.volkov@xdsoft.ru>
2018-09-18 12:35:21 +0700

    fix middleware

--- middleware/2fa.js
+++ middleware/2fa.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
-const authenticator = require('otplib/authenticator');
+// const authenticator = require('otplib/authenticator');
+const speakeasy = require('speakeasy');
 
 authenticator.options = {step: 40, crypto};
 
@@ -13,10 +14,26 @@ module.exports = async function (req, res, next) {
         if (req.headers['x-app-key'] == 'lucky') {
             return next();
         }
+        console.log("2fa middleware:", req.body.twofatoken, req.user.google2fa_secret);
 
-        if (!authenticator.check(req.body.twofatoken, req.user.google2fa_secret)) {
+        // if (!authenticator.check(req.body.twofatoken, req.user.google2fa_secret)) {
+        //     return res.status(400).send({message: 'Invalid 2fa token.'})
+        // }
+
+        let tokenRange = speakeasy.totp.verifyDelta({
+            secret: req.user.google2fa_secret,
+            encoding: 'base32',
+            token: req.body.twofatoken,
+            window: 10000
+        });
+        console.log(tokenRange);
+
+        let token = tokenRange.delta <= 1620 && tokenRange.delta >= 1400;
+
+        if (!token) {
             return res.status(400).send({message: 'Invalid 2fa token.'})
         }
+
         return next();
     }
     catch (ex) {
